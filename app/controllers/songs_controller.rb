9447b9cd0d29c6de830cfaf10bd96cb720d14a3c
Omikron Delair <omikron.dementia@gmail.com>
2014-01-20 00:21:44 -0600

    Initial commit

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,53 @@
+class SongsController < ApplicationController
+  expose :song, attributes: :song_params
+  expose :songs
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+    if song.save(song_params)
+      flash[:notice] = 'Song has been created successfully.'
+      redirect_to song_path song.id
+    else
+      flash[:error] = 'Something went wrong. Try again.'
+      redirect_to new_song_path
+    end
+  end
+
+  def edif
+  end
+
+  def update
+    if song.update_attributes(song_params)
+      flash[:notice] = 'Song has been edited successfully.'
+      redirect_to song_path song.id
+    else
+      flash[:error] = 'Something went wrong. Try again.'
+      redirect_to edit_song_path song.id
+    end
+  end
+
+  def destroy
+    if song.destroy
+      flash[:notice] = 'Song has been destroyed sucessfully.'
+      redirect_to songs_path
+    else
+      flash[:error] = 'Something went wrong. Try again.'
+      redirect_to songs_path
+    end
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title,:artist,:bpm,:delay,:ucs_id)
+  end
+
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UcsXc::Application.config.session_store :cookie_store, key: '_ucs-xc_session'
